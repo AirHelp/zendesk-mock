6281cd9f58ba69a0bcf241983d44ac10d1e95daf
Piotr Brusik <piotr.brusik@airhelp.eu>
2016-08-08 14:47:14 +0200

    Add Tickeds finder get endpoint

--- tickets/tickets.go
+++ tickets/tickets.go
@@ -1,9 +1,20 @@
 package tickets
 
 import (
+	"encoding/json"
+	"strconv"
+
 	"github.com/AirHelp/zendesk-mock/utils"
 	"github.com/go-martini/martini"
 )
+
+type response struct {
+	Ticket ticket `json:"ticket"`
+}
+type ticket struct {
+	Id      int    `json:"id"`
+	Subject string `json:"subject"`
+}
 
 // New creates new ticket
 func New(params martini.Params) (int, string) {
@@ -12,5 +23,14 @@ func New(params martini.Params) (int, string) {
 
 // Find finds ticket
 func Find(params martini.Params) (int, string) {
-	return utils.NotImplemented()
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		return 404, err.Error()
+	}
+	response := response{ticket{id, "Anything from Zendesk"}}
+	bytes, err := json.Marshal(response)
+	if err != nil {
+		return 500, err.Error()
+	}
+	return 200, string(bytes)
 }
