6ed279394a03dc55bf8d577ce17d3eea2bf57dee
Sebastian Nowak <sebastian.nowak@airhelp.com>
2017-02-14 10:32:04 +0100

    Introduce path for updating tickets

--- server.go
+++ server.go
@@ -13,6 +13,7 @@ func main() {
 	m := martini.Classic()
 	m.Post("/api/v2/tickets", tickets.New)
 	m.Get(tickets.TicketsFindURI+":id", tickets.Find)
+	m.Put(tickets.TicketsFindURI+":id", tickets.Find)
 	m.Get(users.UsersFindPath+":id", users.Find)
 	port := ":8080"
 	if len(os.Args) > 1 {
