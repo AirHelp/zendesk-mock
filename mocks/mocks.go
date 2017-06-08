3351b22f220de1a51d1bf597bf6ff01a67bae8de
bombinsky <ba@airhelp.eu>
2017-06-08 11:52:33 +0200

    Tickets endpoint refactoring

--- mocks/mocks.go
+++ mocks/mocks.go
@@ -37,3 +37,25 @@ func GroupMembership(userId int, idOffset int) api.GroupMembership {
 func Group(id int, name string) api.GroupEnvelope {
 	return api.GroupEnvelope{api.Group{Id: id, Name: name}}
 }
+
+func Ticket(id int, subject string) api.TicketEnvelope {
+	customs := []api.CustomField{
+		api.CustomField{28367069, "GF4534"},                                                //reference
+		api.CustomField{28518725, "delayed"},                                               //type
+		api.CustomField{28367089, "600"},                                                   //comp_per_pass
+		api.CustomField{28518845, "600.0"},                                                 //comp_per_pass_decimal
+		api.CustomField{28518825, "600"},                                                   //comp_total
+		api.CustomField{28367689, "600.0"},                                                 //compt_total_decimal
+		api.CustomField{28367989, "en"},                                                    //locale
+		api.CustomField{28368009, "ch_web"},                                                //channel
+		api.CustomField{28518885, "John Doe"},                                              //pass1
+		api.CustomField{28518905, "John F. Kennedy International Airport, New York (JFK)"}, //departure
+		api.CustomField{28368029, "Tegel Airport, Berlin (TXL)"},                           //arrival
+		api.CustomField{28518945, "LOT - Polish Airlines (LO)"},                            //airline_name_and_code
+		api.CustomField{28368069, "554"},                                                   //flight_no
+		api.CustomField{28368089, "2017-03-13"},                                            //date
+		api.CustomField{28518965, "LO"},                                                    //airline_code
+		api.CustomField{29960329, ""}}                                                      //linked_ticket
+
+	return api.TicketEnvelope{api.Ticket{id, subject, customs}}
+}
--- tickets/tickets_test.go
+++ tickets/tickets_test.go
@@ -2,90 +2,41 @@ package tickets_test
 
 import (
 	"encoding/json"
-	"net/http"
-	"net/http/httptest"
-	"strings"
-	"testing"
-	"time"
-
+	"github.com/AirHelp/zendesk-mock/api"
+	"github.com/AirHelp/zendesk-mock/test"
 	"github.com/AirHelp/zendesk-mock/tickets"
+	"net/http/httptest"
+	"testing"
 )
 
-func TestTicketsNew(t *testing.T) {
-	// Create a request to pass to our handler.
-	req, err := http.NewRequest("POST", "/api/v2/tickets", strings.NewReader(`{"ticket":{"subject":"sample subject"}}`))
-	if err != nil {
-		t.Fatal(err)
-	}
+func TestShow(t *testing.T) {
+	var response = test.RecordGet("/api/v2/tickets/:id", "/api/v2/tickets/123", "", tickets.Show)
 
-	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(tickets.New)
-
-	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
-	// directly and pass in our Request and ResponseRecorder.
-	handler.ServeHTTP(rr, req)
-
-	// Check the status code is what we expect.
-	if status := rr.Code; status != 201 {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, 201)
-	}
-
-	// Check the response body is what we expect.
-	timestampID := int(time.Now().Unix())
-	var ticketResponse tickets.Response
-	dec := json.NewDecoder(rr.Body)
-	err = dec.Decode(&ticketResponse)
-	if err != nil {
-		t.Errorf("Unable to unmarshal response: %v", rr.Body.String())
-	}
-	if ticketResponse.Ticket.ID < timestampID {
-		t.Errorf("Wrong ID in response: %v", ticketResponse.Ticket.ID)
-	}
+	test.IsExpectedToRespondWithCode(t, response, 200)
+	test.IsExpectedToNotBeBlank(t, receivedTicket(response).Subject)
 }
 
-func TestTicketsFindGet(t *testing.T) {
-	cases := []struct {
-		uri, expected string
-		code          int
-	}{
-		{
-			tickets.TicketsFindURI + "123",
-			`{"ticket":{"id":123,"subject":"Anything from Zendesk","comment":"","custom_fields":[{"id":28367069,"value":"GF4534"},{"id":28518725,"value":"delayed"},{"id":28367089,"value":"600"},{"id":28518845,"value":"600.0"},{"id":28518825,"value":"600"},{"id":28367689,"value":"600.0"},{"id":28367989,"value":"en"},{"id":28368009,"value":"ch_web"},{"id":28518885,"value":"John Doe"},{"id":28518905,"value":"John F. Kennedy International Airport, New York (JFK)"},{"id":28368029,"value":"Tegel Airport, Berlin (TXL)"},{"id":28518945,"value":"LOT - Polish Airlines (LO)"},{"id":28368069,"value":"554"},{"id":28368089,"value":"2017-03-13"},{"id":28518965,"value":"LO"},{"id":29960329,"value":""}]}}`,
-			200,
-		},
-		{
-			tickets.TicketsFindURI,
-			"",
-			404,
-		},
-	}
-	for _, c := range cases {
-		// Create a request to pass to our handler.
-		req, err := http.NewRequest("GET", c.uri, nil)
-		if err != nil {
-			t.Fatal(err)
-		}
+func TestCreate(t *testing.T) {
+	var response = test.RecordPost("/api/v2/tickets", "/api/v2/tickets", body(), tickets.Create)
 
-		// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-		rr := httptest.NewRecorder()
-		handler := http.HandlerFunc(tickets.Find)
+	test.IsExpectedToRespondWithCode(t, response, 201)
+	test.IsExpectedToNotBeBlank(t, receivedTicket(response).Subject)
+}
 
-		// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
-		// directly and pass in our Request and ResponseRecorder.
-		handler.ServeHTTP(rr, req)
+func TestUpdate(t *testing.T) {
+	var response = test.RecordPut("/api/v2/tickets/:id", "/api/v2/tickets/1", body(), tickets.Update)
 
-		// Check the status code is what we expect.
-		if rr.Code != c.code {
-			t.Errorf("handler returned wrong status code: got %v want %v",
-				rr.Code, c.code)
-		}
+	test.IsExpectedToRespondWithCode(t, response, 200)
+	test.IsExpectedToNotBeBlank(t, receivedTicket(response).Subject)
+}
 
-		// Check the response body is what we expect.
-		if rr.Body.String() != c.expected {
-			t.Errorf("handler returned unexpected body: got %v want %v",
-				rr.Body.String(), c.expected)
-		}
-	}
+func body() string {
+	return `{"ticket":{"subject":"Subject","comment":{"body":"Body"}}}`
+}
+
+func receivedTicket(response *httptest.ResponseRecorder) api.Ticket {
+	var envelope api.TicketEnvelope
+	dec := json.NewDecoder(response.Body)
+	dec.Decode(&envelope)
+	return envelope.Ticket
 }
