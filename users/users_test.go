7406861b5f49ec22a7543a1dc642c126c3bc0f10
bombinsky <ba@airhelp.eu>
2017-06-08 14:24:06 +0200

    Uses RecordMethid

--- users/users_test.go
+++ users/users_test.go
@@ -7,13 +7,13 @@ import (
 )
 
 func TestUsersShow(t *testing.T) {
-	var response = test.RecordMethod("/api/v2/users/:id", "/api/v2/users/123", "", "GET", users.Show)
+	var response = test.RecordMethod("/api/v2/users/:id", "/api/v2/users/123", "", test.Get, users.Show)
 
 	test.IsExpectedToRespondWithCode(t, response, 200)
 }
 
 func TestUsersShowNotExists(t *testing.T) {
-	var response = test.RecordMethod("/api/v2/users/:id", "/api/v2/users/wer", "", "GET", users.Show)
+	var response = test.RecordMethod("/api/v2/users/:id", "/api/v2/users/wer", "", test.Get, users.Show)
 
 	test.IsExpectedToRespondWithCode(t, response, 404)
 }
